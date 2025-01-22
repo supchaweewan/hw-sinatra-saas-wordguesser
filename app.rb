e38de83d88f19e0916d95d0c321c036d2abceaa1
Suppanat Chaweewan <supchaweewan@gmail.com>
2025-01-22 08:09:10 +0000

    Added the Hello World bit

--- app.rb
+++ app.rb
@@ -61,6 +61,11 @@ class WordGuesserApp < Sinatra::Base
     ### YOUR CODE HERE ###
     erb :lose # You may change/remove this line
   end
+  
 end
 
-
+class WordGuesserApp
+  post '/new' do
+    "Hello World"
+  end
+end
